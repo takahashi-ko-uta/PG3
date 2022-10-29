15cd7c07163fd73bcdcb9a94a87aeb908dc0f40f
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2022-10-29 21:31:13 +0900

    kannsei

--- main.cpp
+++ main.cpp
@@ -1,58 +1,56 @@
 #include <stdio.h>
 #include <Windows.h>
+#include <functional>
 #include <time.h>
 
 typedef void (*PFunc)(int*);
 
-void callback1(int *s)
+void callback1(int* s)
 {
-	printf("%d秒後に実行\n", *s);
+	printf("%d秒後に実行されるよ\n", *s);
 }
 
-void setTimeout(PFunc p,int second)
+void setTimeout(PFunc p, int second)
 {
 	Sleep(second * 1000);
 	p(&second);
 }
 
-
-int main()
+int main(int argc,const char *argv[])
 {
-	srand(time(nullptr));
-	int number = rand() % 6 + 1;//1～6のランダムな数字を生成
 	int num;
+	std::function<int()>dice = []()
+	{
+		srand(time(nullptr));
+		int x = rand() % 6 + 1;
+		return x;
+	};
+
+	auto result = [](int y)
+	{
+		if(y % 2== 0)
+		{
+			return printf("あなたの予想 : 偶数\n");
+		}
+		else
+		{
+			return printf("あなたの予想 : 奇数\n");
+		}
+	};
+	
 
 	printf("strat\n");
 	printf("サイコロの目を予想\n偶数 = 0 奇数 = 1を入力\n");
 	scanf_s("%d", &num);
-	if(num == 0)
-	{
-		printf("予想 : 偶数\n");
-	}
-	else
-	{
-		printf("予想 : 奇数\n");
-	}
 	
+	result(num);
+
 	PFunc p;
 	p = callback1;
 	setTimeout(p, 3);
 
-	if((number%2) == 0)//偶数
-	{
-		printf("正解 : 丁\n");
-	}
-	else               //奇数
-	{
-		printf("正解 : 半\n");
-	}
-	
-
-	
-
-	
-	
-	return (0);
+	printf("サイコロの目:%d\n", dice());
+	return 0;
 }
 
 
