783bcc65204067a3577ed3c24c490b569fe223fc
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2022-11-01 21:12:20 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -26,7 +26,7 @@ int main(int argc,const char *argv[])
 		return x;
 	};
 
-	auto result = [](int y)
+	auto Forecasts = [](int y)
 	{
 		if(y % 2== 0)
 		{
@@ -37,19 +37,48 @@ int main(int argc,const char *argv[])
 			return printf("あなたの予想 : 奇数\n");
 		}
 	};
+
+	auto Result = [](int x, int y)//x...予想 ,y...サイコロの目
+	{
+		if((x % 2) == 0)
+		{
+			if((y % 2) == 0)
+			{
+				return printf("正解\n");
+			}
+			else
+			{
+				return printf("不正解\n");
+			}
+		}
+		else if ((x % 2) == 1)
+		{
+			if ((y % 2) == 1)
+			{
+				return printf("正解\n");
+			}
+			else
+			{
+				return printf("不正解\n");
+			}
+		}
+	};
 	
 
 	printf("strat\n");
 	printf("サイコロの目を予想\n偶数 = 0 奇数 = 1を入力\n");
 	scanf_s("%d", &num);
 	
-	result(num);
+	Forecasts(num);
 
 	PFunc p;
 	p = callback1;
 	setTimeout(p, 3);
 
-	printf("サイコロの目:%d\n", dice());
+	int a = dice();//ダイスの結果を保存
+	printf("サイコロの目:%d\n", a);
+	Result(num, a);
+
 	return 0;
 }
 
