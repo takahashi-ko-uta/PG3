138cb4bed8ab40a00eeabdec03da8e51693713ce
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2022-11-07 15:57:40 +0900

    修正

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ void setTimeout(PFunc p,int second)
 int main()
 {
 	srand(time(nullptr));
-	int number = rand() % 6 + 1;//1～6のランダムな数字を生成
+	int dice = rand() % 6 + 1;//1～6のランダムな数字を生成
 	int num;
 
 	printf("strat\n");
@@ -38,13 +38,28 @@ int main()
 	p = callback1;
 	setTimeout(p, 3);
 
-	if((number%2) == 0)//偶数
+	printf("出たサイコロの目:%d\n", dice);
+	if((dice%2) == 0)//偶数
 	{
-		printf("正解 : 丁\n");
+		if(num == 0)
+		{
+			printf("正解\n");
+		}
+		else
+		{
+			printf("不正解\n");
+		}
 	}
 	else               //奇数
 	{
-		printf("正解 : 半\n");
+		if (num == 1)
+		{
+			printf("正解\n");
+		}
+		else
+		{
+			printf("不正解\n");
+		}
 	}
 	
 
