41296e910ed058812a2cb14c03bdfecab957604f
KoRoTa-school@outlook.jp <KoRoTa-school@outlook.jp>
2022-11-29 15:29:08 +0900

    とりあえず完成

--- main.cpp
+++ main.cpp
@@ -4,54 +4,60 @@
 #include <iostream>
 using namespace std;
 
+void* malloc(size_t size);
+
+typedef struct cell{
+	int val;
+	struct cell* next;
+}CELL;
+
+void create(CELL* head, int x);
+void index(CELL* head);
+
 int main()
 {	
-	printf("==1970==\n");
-	std::list<string>lst = {
-	"oosaki","gotannda","meguro","ebisu","sibuya","harazyuku",
-	"yoyogi","sinnzyuku","sinnookubo","takadanobaba","meziro",
-	"ikebukuro","ootuka","sugamo","komagome","tabata",
-	"nippori","uguisudani","ueno","okatimati","akihabara","kannda",
-	"toukyou","yuurakutyou","sinnbasi","hamamatutyou","tamata","sinagawa"
-	};
+	int val;
+	CELL head;
+	head.next = nullptr;
 	
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		std::cout << *itr << "\n";
+	while (true){
+		printf("追加する値を入力:");
+		scanf_s("%d", &val);
+
+		//手順3
+		create(&head, val);
+
+		//手順４
+		index(&head);
+
 	}
-
-	printf("==2019==\n");
-
-	for(std::list<string>::iterator itr = lst.begin();itr != lst.end();++itr){	//西日暮里を追加
-		if(*itr == "nippori")
-		{
-			itr = lst.insert(itr, "nisinippori");
-			++itr;
-		}
-	}
-
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
-
-	printf("==2022==\n");
-
-	for (std::list<string>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {	//ゲートウェイを追加
-		if (*itr == "sinagawa")
-		{
-			itr = lst.insert(itr, "takanawage-toway");
-			++itr;
-		}
-	}
-
-	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
-		std::cout << *itr << "\n";
-	}
-
-	system("pause");
+	
 	return 0;
 }
 	
 
+void create(CELL* head, int x) {
+	CELL* cell;
+	//新規作成するセル分のメモリを確保する
+	cell = (CELL*)malloc(sizeof(CELL));
+
+	cell->val = x;
+	cell->next = nullptr;
+	//最後(最新)のセルのアドレスの1つ目の処理は引数から持ってきた
+	//リストのうち最初のセルのアドレスが該当する
+	while (head->next != nullptr){
+		head = head->next;
+	}
+	head->next = cell;
+}
+
+void index(CELL* head) {
+	while (head->next != nullptr){
+		head = head->next;
+		printf("%d\n", head->val);
+	}
+	printf("-------------------------------\n");
+}
 
 
 
